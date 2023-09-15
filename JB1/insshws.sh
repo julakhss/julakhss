393d4d5919a1e2483c9fe17312220f5b72d26664
julakhss <144143635+julakhss@users.noreply.github.com>
2023-09-15 23:34:55 +0800

    Update insshws.sh

--- JB1/insshws.sh
+++ JB1/insshws.sh
@@ -5,20 +5,20 @@ cd
 apt install python -y
 
 #Install Script Websocket-SSH Python
-wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/openssh.py
+#wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/openssh.py
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/ws-stunnel.txt
 wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/ovpn.py
 
 #izin permision
-chmod +x /usr/local/bin/edu-proxy
+#chmod +x /usr/local/bin/edu-proxy
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 chmod +x /usr/local/bin/ws-ovpn
 
 
 #System OpenSSH Websocket-SSH Python
-wget -O /etc/systemd/system/edu-proxy.service https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/http.service && chmod +x /etc/systemd/system/edu-proxy.service
+#wget -O /etc/systemd/system/edu-proxy.service https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/http.service && chmod +x /etc/systemd/system/edu-proxy.service
 
 #System Dropbear Websocket-SSH Python
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
@@ -33,9 +33,9 @@ wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/ju
 #
 systemctl daemon-reload
 #Enable & Start & Restart ws-openssh service
-systemctl enable ws-openssh.service
-systemctl start ws-openssh.service
-systemctl restart ws-openssh.service
+#systemctl enable ws-openssh.service
+#systemctl start ws-openssh.service
+#systemctl restart ws-openssh.service
 
 #Enable & Start & Restart ws-dropbear service
 systemctl enable ws-dropbear.service

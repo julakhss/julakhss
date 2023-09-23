80ee7a86729da5168b8c4dfc482d0a1f04c706dc
julakhss <144143635+julakhss@users.noreply.github.com>
2023-09-23 23:36:22 +0800

    Add files via upload

---
+++ JB1/insshws.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#installer Websocker tunneling 
+
+cd
+apt install python -y
+
+#Install Script Websocket-SSH Python
+wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/openssh.py
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/ws-stunnel.txt
+
+#izin permision
+chmod +x /usr/local/bin/edu-proxy
+chmod +x /usr/local/bin/ws-dropbear
+chmod +x /usr/local/bin/ws-stunnel
+
+#System OpenSSH Websocket-SSH Python
+wget -O /etc/systemd/system/edu-proxy.service https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/http.service && chmod +x /etc/systemd/system/edu-proxy.service
+
+#System Dropbear Websocket-SSH Python
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
+
+#System SSL/TLS Websocket-SSH Python
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/julakhss/julakhss/main/JB1/ws-stunnel.service.txt && chmod +x /etc/systemd/system/ws-stunnel.service
+
+systemctl daemon-reload
+#Enable & Start & Restart ws-openssh service
+systemctl enable edu-proxy.service
+systemctl start edu-proxy.service
+systemctl restart edu-proxy.service
+
+#Enable & Start & Restart ws-dropbear service
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+
+#Enable & Start ws-stunnel service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service

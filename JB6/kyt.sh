8f06e4d249cd742f65ae79b10c1e4575c203709a
julakhss <144143635+julakhss@users.noreply.github.com>
2023-10-09 14:12:30 +0800

    Update kyt.sh

--- JB6/kyt.sh
+++ JB6/kyt.sh
@@ -9,13 +9,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/ppnhss/vip/main/bot/bot.zip
+wget https://raw.githubusercontent.com/julakhss/julakhss/main/JB6/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/ppnhss/vip/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/julakhss/julakhss/main/JB6/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 

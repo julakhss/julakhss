d314157db8288e7785c0d3581c43ce8f87d1cba0
julakhss <144143635+julakhss@users.noreply.github.com>
2023-10-11 15:50:14 +0800

    Update update.sh

--- JB7/update.sh
+++ JB7/update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/ppnhss/haproxy/main/menu/menu.zip
+    wget "https://raw.githubusercontent.com/julakhss/julakhss/main/JB6/menu.zip"
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin

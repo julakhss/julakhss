c9ca8a6a1b8d04b8d719cd7159b7e25f899fc9a0
julakhss <144143635+julakhss@users.noreply.github.com>
2023-10-22 22:08:33 +0800

    Update update.sh

--- JB7/update.sh
+++ JB7/update.sh
@@ -47,7 +47,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget "https://raw.githubusercontent.com/julakhss/julakhss/main/JB7/menu.zip"
+    wget "https://raw.githubusercontent.com/julakhss/julakhss/main/JB6/menu.zip"
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin

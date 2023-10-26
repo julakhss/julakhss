c8fb64ad390feb5267d8cee28d055be468b8f7a2
julakhss <144143635+julakhss@users.noreply.github.com>
2023-10-26 15:33:36 +0800

    Update ins-xray.sh

--- JB5/ins-xray.sh
+++ JB5/ins-xray.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-# =========================================
-# Quick Setup | Script Setup Manager
-# Edition : Stable Edition V1.0
-# Auther  : Julak Bantur
-# (C) Copyright 2023
-# =========================================
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/julak/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m"
+COLOR1="$(cat /etc/julak/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/julak/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
 # // Export Color & Information
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
@@ -30,7 +34,7 @@ export UNDERLINE="\e[4m"
 mkdir /user/curent > /dev/null 2>&1
 touch /user/current
 clear
-echo "IP=$domain" > /var/lib/ipvps.conf
+echo "IP=$domain" > /var/lib/julak/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
 else
